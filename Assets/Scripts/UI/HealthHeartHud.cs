a661ebc0d2567f2570bb48298886a8ed24a2d7ac
sagalajerome <jerome.sagala22@gmail.com>
2022-02-21 06:07:56 +0800

    Will add health heart

---
+++ Assets/Scripts/UI/HealthHeartHud.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHeartHud : MonoBehaviour
+{
+    [SerializeField] List<Image> lives;
+
+    int healthCount = 0;
+    Color grayScaleColor;
+
+    EventBroadcaster eventBroadcaster;
+
+    //public void Instance
+
+}
